9ac3f815906bd5d6404dc07f23df3a0cd02094f6
Bharathvaaj25 <Bharathwaj@thestackly.com>
2026-03-26 16:51:45 +0530

    Add files via upload

---
+++ Mini project5.py
@@ -0,0 +1,15 @@
+
+
+
+visitors = set()
+
+visitors.add("Arun")
+visitors.add("Bala")
+visitors.add("Charan")
+visitors.add("Arun")   
+visitors.add("Divya")
+visitors.add("Bala")   
+
+print("Visitors:", visitors)
+
+print("Total Unique Visitors:", len(visitors))
